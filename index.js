91c77664db88ceed4f8c6279010240f94bb5f2d3
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-02-13 16:23:31 +0100

    try to connect json file to server

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+const filePath = path.join(__dirname, "menu.json");
+
 const users = [
   { id: 1, name: "John Doe" },
   { id: 2, name: "Jane Doe" },
   { id: 3, name: "Michael Johnson" },
 ];
+
+app.get("/menu", async (req, res) => {
+  try {
+    const menu = await fs.promises.readFile(filePath, "utf8");
+    res.json(JSON.parse(menu));
+  } catch (error) {
+    console.error("Error reading menu:", error);
+    res.status(500).send("Error reading menu");
+  }
+})
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send('menu');
 });
 
-app.get("/users", (req, res) => {
-  res.json(users);
-});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
