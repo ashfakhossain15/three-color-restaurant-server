cf901fad016b883817b2249c75fc130f9dcf4f51
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-03-12 16:37:06 +0100

    got every menu item by id

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const app = express();
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 
 const menu = require("./data/menu.json");
-
 app.get("/", (req, res) => {
   res.send("Welcome to the Food Delivery App!");
 });
@@ -16,6 +15,31 @@ app.get("/menu", (req, res) => {
   res.send(menu);
 });
 
+app.get("/menu/menu/pizza/:id", (req, res) => {
+  const id = req.params.id;
+  const pizza = menu.menu.pizza.find((pizza) => pizza.id === parseInt(id));
+});
+
+app.get("/menu/menu/pasta/:id", (req, res) => {
+  const id = req.params.id;
+  const pasta = menu.menu.pasta.find((pasta) => pasta.id === parseInt(id));
+});
+app.get("/menu/menu/calzone/:id", (req, res) => {
+  const id = req.params.id;
+  const calzone = menu.menu.calzone.find(
+    (calzone) => calzone.id === parseInt(id)
+  );
+});
+app.get("/menu/menu/sandwiches/:id", (req, res) => {
+  const id = req.params.id;
+  const sandwiches = menu.menu.sandwiches.find(
+    (sandwiches) => sandwiches.id === parseInt(id)
+  );
+});
+app.get("/menu/menu/fries/:id", (req, res) => {
+  const id = req.params.id;
+  const fries = menu.menu.fries.find((fries) => fries.id === parseInt(id));
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
